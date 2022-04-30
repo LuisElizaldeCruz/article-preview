b5a3388be7f00ad49e614cfc7c7ed80e0764f2e5
LuisElizaldeCruz <luis.elizalde.dev@gmail.com>
2022-04-30 01:39:00 -0500

    se agrego la interactividad del boton para modo escritorio

--- js/script.js
+++ js/script.js
@@ -1,15 +1,33 @@
 
-document.getElementById("boton").addEventListener("click", mostrarDato);
+document.getElementById("btn").addEventListener("click", mostrarVentana);
 
+/*
 function mostrarDato(){
     console.log("link funcionando");
     let info=document.getElementById("perfil-social");
-    /*padre = info.parentNode;
-    padre.removeChild(info);*/
+    //padre = info.parentNode;
+    //padre.removeChild(info);
     info.style.backgroundColor="hsl(217, 19%, 35%)";
 
     //color de fondo icono
 
     let btn = document.getElementById("boton");
     btn.style.backgroundColor="hsl(214, 17%, 51%)";
+}
+*/
+/*
+console.log("link funcionando");
+let ventanM = document.getElementById("ventana");
+let triangulo = document.getElementById("triangle");
+
+function mostrarVentana(){
+    
+    ventanM.style.display = "block";
+    triangulo.style.display = "block";
+    
+}*/
+
+function mostrarVentana(){
+    let ventana = document.getElementById("ventana").classList.toggle("ocultar");
+    let triangulo = document.getElementById("triangle").classList.toggle("ocultar");
 }
